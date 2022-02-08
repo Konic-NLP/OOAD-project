fb688bda548034f2f674fc101d5703e5e471951e
Sijia(Konic) Ge <sijiage007@gmail.com>
2022-02-07 20:09:16 -0700

    register class  2/7 Sijia

--- register.java
+++ register.java
@@ -1,5 +1,39 @@
 public class register {
 
+    private int moneysum;// sum of money in the register
+
+
+    /*
+
+    add the money to the register
+     */
+
+    public void addmoney(int amount){
+
+        this.moneysum+=amount;
+
+
+    }
+
+    // get to know the  money
+    public int getMoneysum() {
+
+        return this.moneysum;
+
+
+    }
+    // if a seller customer sells the item to the store, we need to deduct the money from the register
+     public void deductmoney(Items items){
+
+
+        //the price is determined by the
+        this.moneysum-=items.getPurchasePrice;
+
+     }
+
+
+
+
 
 
 
