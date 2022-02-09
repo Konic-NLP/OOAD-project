8ea6cdbe9e0bd1c85991ea20c03ae1db3fe82840
ravenouse <zhwa3087@colorado.edu>
2022-02-08 23:59:14 -0700

    Brother Eric

--- Classpicker.java
+++ Classpicker.java
@@ -22,32 +22,32 @@ public class Classpicker {
         classes.add(PracticeAmps.class);
         classes.add(Cables.class);
         classes.add(Strings.class);
+    }
 
-        public static Items MakeRandomInstance(){
-            Items a;
-            int pick;
-            Random r = new Random();
-            pick =r.nextInt(15);
-            switch (pick) {
-                case 0 -> new PlayerCD();
-                case 1 -> new MusicCD();
-                case 3 -> new MP3();
-                case 4 -> new Guitar();
-                case 5 -> new Bass();
-                case 6 -> new Mandolin();
-                case 7 -> new Hats();
-                case 8 -> new Shirts();
-                case 9 -> new Bandanas();
-                case 10 -> new PracticeAmps();
-                case 11 -> new Cables();
-                case 12 -> new Strings();
-                default -> {
-                    a = null;
-                    System.out.println("whoops");
-                }
-            }
-            return a;
+    public Items MakeRandomInstance() {
+        Items a;
+        int pick;
+        Random r = new Random();
+        pick = r.nextInt(15);
+        switch (pick) {
+            case 0 -> new PlayerCD();
+            case 1 -> new MusicCD();
+            case 3 -> new MP3();
+            case 4 -> new Guitar();
+            case 5 -> new Bass();
+            case 6 -> new Mandolin();
+            case 7 -> new Hats();
+            case 8 -> new Shirts();
+            case 9 -> new Bandanas();
+            case 10 -> new PracticeAmps();
+            case 11 -> new Cables();
+            case 12 -> new Strings();
+            default -> {
+                a = null;
+                System.out.println("whoops");
             }
         }
+        return a;
     }
+}
 
